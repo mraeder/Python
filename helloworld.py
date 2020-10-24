183eb502dea835f2413390e59031386269450aaf
Marisa Raeder <mraeder11@gmail.com>
2020-10-24 17:45:10 -0500

    Python1

---
+++ helloworld.py
@@ -0,0 +1,7 @@
+#! usr/bin/env/python3
+
+print('Content-type: text/html')
+print()
+print('Hello World!')
+
+
---
+++ index.php
@@ -0,0 +1,70 @@
+<html>
+	<head>	
+	
+	<title>The Poopluster Within Youuu</title>
+	
+	<h1><p id="top">The Wanderluster Within You</h1>
+	
+	</head>
+	
+	<body>  
+	
+	<p>Traveling <b>sets us free</b>. Check out the video below!</p>
+	
+	<p><i>Why World Travel Is Awesome:</i></P>
+	
+	<iframe width="480" height="290" src="https://www.youtube.com/embed/yQCpe8ArgYo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+
+		<p> If you identify with any of these, we want to <u>hear from you!</u><p>
+		
+		<ul>
+			<li>Go-getter</li>
+			<li>Trailblazer</li>
+			<li>Backpacker</li>
+			<li>Hippie</li>
+			<li>Solo Traveler</li>
+		</ul>
+		
+		<img src="wander.jpg"width="275"height="200">
+		
+		<p><b>Tell us a little more about you!</b></p>
+		<form>
+		<p>Name:<input type="text" placeholder="your name"><p>
+		<p>Email:<input type="text" placeholder="your email"><p>
+		
+		<br>
+		<p><b>Level of Travel Experience</b></p>
+		
+		<p><input type="radio" name="exp">I'm starting to get out there!(1-3 Countries)</p>
+		<p><input type="radio" name="exp">I've made quite a few international trips! (3-12 Countries)</p>
+		<p><input type="radio" name="exp">I know the world like the back of my hand! (12+ Countries)</p>
+		</br>
+		
+		<br>
+		<p><b>Would you like to be contacted with customized travel tips?</b></p>
+			<select>
+				<option>Yes</option>
+				<option>No</option>
+			</select>
+		</br>
+		
+		<br>
+		<p><input type="submit" value="SEND IT"></p>
+		</br>
+		
+		<br>
+		<h3>Get your next trip going by checking out flights!</h3>
+		<p><a href="https://www.kayak.com/">Kayak.com</a></p>
+		
+		<p><img src="someplace.jpg"width="275"height="200"><p>
+		
+		<br>
+		<p><a href="#top">Zoom back to top</a></p>
+		</br>
+	
+		<iframe src="https://www.gysiwerks.com/">Check out my friend's site</iframe></p>
+		
+	</body>
+
+</html>
